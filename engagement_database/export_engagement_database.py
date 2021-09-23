5b20d2033c2e16fb23e1d97a432c97ab16d8e584
Alexander Simpson <as2388@cam.ac.uk>
2021-09-23 12:29:29 +0100

    Incrementally export messages and history entries

--- engagement_database/export_engagement_database.py
+++ engagement_database/export_engagement_database.py
@@ -4,10 +4,14 @@ import json
 
 from core_data_modules.logging import Logger
 from engagement_database import EngagementDatabase
+from google.cloud import firestore
+from google.cloud.firestore_v1 import DocumentReference
 
 from storage.google_cloud import google_cloud_utils
 
 log = Logger(__name__)
+
+BATCH_SIZE = 500
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Exports an engagement database to a zipped json file")
@@ -44,19 +48,59 @@ if __name__ == "__main__":
     ))
 
     engagement_db = EngagementDatabase.init_from_credentials(engagement_database_credentials, database_path)
-    messages = engagement_db.get_messages()
 
-    export = []
-    for msg in messages:
-        export.append({"type": "message", "data": msg.to_dict(serialize_datetimes_to_str=True)})
+    with open("export.json", "w") as f:
+        log.info(f"Exporting all messages...")
+        # Paginate the export because Firestore returns incomplete results when making queries that have a long run time
+        total_messages = 0
+        batch_messages = engagement_db.get_messages(firestore_query_filter=lambda q: q.order_by("message_id").limit(BATCH_SIZE))
+        while len(batch_messages) > 0:
+            # Process this batch by serializing and writing to disk
+            total_messages += len(batch_messages)
+            log.info(f"Fetched {len(batch_messages)} messages in this batch ({total_messages} total)")
 
-    log.info(f"Converting fetched data to zipped jsonl for export...")
-    jsonl_blob = ""
-    for item in export:
-        jsonl_blob += json.dumps(item) + "\n"
-    export_compressed = gzip.compress(bytes(jsonl_blob, "utf-8"))
+            for msg in batch_messages:
+                json.dump({"type": "message", "data": msg.to_dict(serialize_datetimes_to_str=True)}, f)
+                f.write("\n")
 
-    if gzip_export_file_path is not None:
-        log.warning(f"Writing export to local disk at '{gzip_export_file_path}'...")
-        with open(gzip_export_file_path, "wb") as f:
-            f.write(export_compressed)
+            # Fetch the next batch
+            last_message = batch_messages[-1]
+            batch_messages = engagement_db.get_messages(
+                firestore_query_filter=lambda q: q.order_by("message_id").start_after(last_message.to_dict()).limit(BATCH_SIZE)
+            )
+        log.info(f"Exported {total_messages} messages")
+
+        log.info(f"Exporting history...")
+        total_history_entries = 0
+        batch_history_entries = engagement_db.get_history(firestore_query_filter=lambda q: q.order_by("history_entry_id").limit(BATCH_SIZE))
+        while len(batch_history_entries) > 0:
+            # Process this batch by serializing and writing to disk
+            total_history_entries += len(batch_history_entries)
+            log.info(f"Fetched {len(batch_history_entries)} history entries in this batch ({total_history_entries} total")
+
+            for entry in batch_history_entries:
+                json.dump({"type": "history_entry", "data": entry.to_dict(serialize_datetimes_to_str=True)}, f)
+                f.write("\n")
+
+            # Fetch the next batch
+            last_entry = batch_history_entries[-1]
+            batch_history_entries = engagement_db.get_history(
+                firestore_query_filter=lambda q: q.order_by("history_entry_id").start_after(last_entry.to_dict()).limit(BATCH_SIZE)
+            )
+        log.info(f"Exported {total_history_entries} history entries")
+
+
+    # export = []
+    # for msg in messages:
+    #     export.append({"type": "message", "data": msg.to_dict(serialize_datetimes_to_str=True)})
+    #
+    # log.info(f"Converting fetched data to zipped jsonl for export...")
+    # jsonl_blob = ""
+    # for item in export:
+    #     jsonl_blob += json.dumps(item) + "\n"
+    # export_compressed = gzip.compress(bytes(jsonl_blob, "utf-8"))
+    #
+    # if gzip_export_file_path is not None:
+    #     log.warning(f"Writing export to local disk at '{gzip_export_file_path}'...")
+    #     with open(gzip_export_file_path, "wb") as f:
+    #         f.write(export_compressed)

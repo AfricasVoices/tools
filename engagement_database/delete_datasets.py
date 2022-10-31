11c6ca1f06deebf67517368d5fd9e2c967e54454
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-10-31 11:34:04 +0300

    add log statement

--- engagement_database/delete_datasets.py
+++ engagement_database/delete_datasets.py
@@ -74,6 +74,7 @@ if __name__ == "__main__":
             engagement_db.delete_doc(f"messages/{msg.message_id}")
 
             history_entries = engagement_db.get_history_for_message(msg.message_id)
-            log.warning(f"Deleting {len(history_entries)} engagement db message's history entries")
+            log.warning(f"Found {len(history_entries)} engagement db message's history entries")
             for history_entry in history_entries:
+                log.warning(f"Deleting history entry with id {history_entry.history_entry_id}")
                 engagement_db.delete_doc(f"history/{history_entry.history_entry_id}")

3a24241ae067c0d091b038d02479dbff9313b4b7
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-10-31 10:25:23 +0300

    delete dataset history and coda message

--- engagement_database/delete_datasets.py
+++ engagement_database/delete_datasets.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 
+from coda_v2_python_client.firebase_client_wrapper import CodaV2Client
 from core_data_modules.logging import Logger
 from storage.google_cloud import google_cloud_utils
 from engagement_database import EngagementDatabase
@@ -25,7 +26,8 @@ if __name__ == "__main__":
     google_cloud_credentials_file_path = args.google_cloud_credentials_file_path
     engagement_database_credentials_file_url = args.engagement_database_credentials_file_url
     database_path = args.database_path
-    engagement_db_datasets = args.engagement_db_datasets
+    coda_credentials_file_url = args.coda_credentials_file_url
+    engagement_db_to_coda_dataset = args.engagement_db_to_coda_dataset
 
     log.info("Downloading Firestore engagement database credentials...")
     engagement_database_credentials = json.loads(google_cloud_utils.download_blob_to_string(
@@ -33,15 +35,28 @@ if __name__ == "__main__":
         engagement_database_credentials_file_url
     ))
 
-    engagement_db = EngagementDatabase.init_from_credentials(engagement_database_credentials, database_path)
+    log.info("Downloading Firestore coda credentials...")
+    coda_credentials = json.loads(google_cloud_utils.download_blob_to_string(
+        google_cloud_credentials_file_path,
+        coda_credentials_file_url
+    ))
 
-    for engagement_db_dataset in engagement_db_datasets:
+    engagement_db = EngagementDatabase.init_from_credentials(engagement_database_credentials, database_path)
+    coda = CodaV2Client.init_client(coda_credentials)
+
+    for engagement_db_dataset, coda_dataset in engagement_db_to_coda_dataset.items():
         messages_filter = lambda q: q.where("dataset", "==", engagement_db_dataset)
         messages = engagement_db.get_messages(firestore_query_filter=messages_filter, batch_size=500)
 
         for msg in messages:
+            if msg.coda_id is not None:
+                coda_message = coda.get_dataset_message(coda_dataset, msg.coda_id)
+                assert coda_message is not None
+
             engagement_db.delete_doc(f"messages/{msg.message_id}")
 
             history_entries = engagement_db.get_history_for_message(msg.message_id)
             for history_entry in history_entries:
                 engagement_db.delete_doc(f"history/{history_entry.history_entry_id}")
+
+            coda.delete_dataset_message(coda_dataset, msg.coda_id)

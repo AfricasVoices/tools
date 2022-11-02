dfeea19de775318083b898b3eb437dab027c556f
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-11-02 10:27:55 +0300

    undo coda dataset deletion

--- engagement_database/delete_datasets.py
+++ engagement_database/delete_datasets.py
@@ -1,25 +1,11 @@
 import argparse
 import json
 
-from coda_v2_python_client.firebase_client_wrapper import CodaV2Client
 from core_data_modules.logging import Logger
 from storage.google_cloud import google_cloud_utils
 from engagement_database import EngagementDatabase
 
 log = Logger(__name__)
-
-class keyvalue(argparse.Action):
-    """
-    argparse action to split an argument into KEY=VALUE form
-    on the = and append to a dictionary.
-    """
-    def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, dict())
-          
-        for value in values:
-            key, value = value.split('=')
-            getattr(namespace, self.dest)[key] = value
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Deletes messages for given datasets in engagement database")
@@ -30,14 +16,11 @@ if __name__ == "__main__":
                         help="Path to a Google Cloud service account credentials file to use to access the "
                              "credentials bucket")
     parser.add_argument("engagement_database_credentials_file_url", metavar="engagement-database-credentials-file-url",
-                        help="GS URL to the Firestore credentials file")
+                        help="GS URL to the Engagement firestore database credentials file")
     parser.add_argument("database_path", metavar="database-path",
                         help="Path to the engagement database e.g. engagement_databases/test")
-    parser.add_argument("coda_credentials_file_url", metavar="coda-credentials-file-url",
-                        help="GS URL to the Firestore credentials file")
-    parser.add_argument("engagement_db_to_coda_dataset", nargs="*", metavar="engagement-db-to-coda-dataset",
-                        action=keyvalue, help="Key-value pair e.g. gender/TEST_gender where the key is the engagement db " 
-                                              "dataset and the value is the coda dataset")
+    parser.add_argument("engagement_db_datasets", nargs="+", metavar="engagement-db-datasets",
+                        help="Engagement database datasets to delete")
 
     args = parser.parse_args()
 
@@ -45,8 +28,7 @@ if __name__ == "__main__":
     google_cloud_credentials_file_path = args.google_cloud_credentials_file_path
     engagement_database_credentials_file_url = args.engagement_database_credentials_file_url
     database_path = args.database_path
-    coda_credentials_file_url = args.coda_credentials_file_url
-    engagement_db_to_coda_dataset = args.engagement_db_to_coda_dataset
+    engagement_db_datasets = args.engagement_db_datasets
 
     dry_run_text = "(dry run)" if dry_run else ""
     log.info(f"Deletes messages in given engagement database {dry_run_text}")
@@ -57,26 +39,13 @@ if __name__ == "__main__":
         engagement_database_credentials_file_url
     ))
 
-    log.info("Downloading Firestore coda credentials...")
-    coda_credentials = json.loads(google_cloud_utils.download_blob_to_string(
-        google_cloud_credentials_file_path,
-        coda_credentials_file_url
-    ))
-
     engagement_db = EngagementDatabase.init_from_credentials(engagement_database_credentials, database_path)
-    coda = CodaV2Client.init_client(coda_credentials)
 
-    for engagement_db_dataset, coda_dataset in engagement_db_to_coda_dataset.items():
+    for engagement_db_dataset in engagement_db_datasets:
         messages_filter = lambda q: q.where("dataset", "==", engagement_db_dataset)
         messages = engagement_db.get_messages(firestore_query_filter=messages_filter, batch_size=500)
 
         for msg in messages:
-            if msg.coda_id is not None:
-                coda_message = coda.get_dataset_message(coda_dataset, msg.coda_id)
-                assert coda_message is not None
-                if not dry_run:
-                    coda.delete_dataset_message(coda_dataset, msg.coda_id)
-
             log.warning(f"Deleting engagement db message with message id {msg.message_id}")
             if not dry_run:
                 engagement_db.delete_doc(f"messages/{msg.message_id}")
